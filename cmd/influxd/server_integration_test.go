6b96c30550b2cbaef4ba5ed32139c857fa6e4e21
Cory LaNou <cory@lanou.com>
2015-04-16 16:09:55 -0600

    explicitly specify bind port for graphite tests to properly form IPv6 addresses

--- cmd/influxd/server_integration_test.go
+++ cmd/influxd/server_integration_test.go
@@ -1579,10 +1579,11 @@ func Test_ServerSingleGraphiteIntegration_Default(t *testing.T) {
 	c.Port = 0
 	c.Admin.Enabled = false
 	g := main.Graphite{
-		Enabled:  true,
-		Database: "graphite",
-		Protocol: "TCP",
-		Port:     0,
+		Enabled:     true,
+		Database:    "graphite",
+		Protocol:    "TCP",
+		BindAddress: "127.0.0.1",
+		Port:        0,
 	}
 	c.Graphites = append(c.Graphites, g)
 
@@ -1635,10 +1636,11 @@ func Test_ServerSingleGraphiteIntegration_FractionalTime(t *testing.T) {
 	c.Port = 0
 	c.Admin.Enabled = false
 	g := main.Graphite{
-		Enabled:  true,
-		Database: "graphite",
-		Protocol: "TCP",
-		Port:     0,
+		Enabled:     true,
+		Database:    "graphite",
+		Protocol:    "TCP",
+		BindAddress: "127.0.0.1",
+		Port:        0,
 	}
 	c.Graphites = append(c.Graphites, g)
 
@@ -1692,10 +1694,11 @@ func Test_ServerSingleGraphiteIntegration_ZeroDataPoint(t *testing.T) {
 	c.Port = 0
 	c.Admin.Enabled = false
 	g := main.Graphite{
-		Enabled:  true,
-		Database: "graphite",
-		Protocol: "TCP",
-		Port:     0,
+		Enabled:     true,
+		Database:    "graphite",
+		Protocol:    "TCP",
+		BindAddress: "127.0.0.1",
+		Port:        0,
 	}
 	c.Graphites = append(c.Graphites, g)
 
@@ -1748,9 +1751,10 @@ func Test_ServerSingleGraphiteIntegration_NoDatabase(t *testing.T) {
 	c.Port = 0
 	c.Admin.Enabled = false
 	g := main.Graphite{
-		Enabled:  true,
-		Port:     0,
-		Protocol: "TCP",
+		Enabled:     true,
+		Protocol:    "TCP",
+		BindAddress: "127.0.0.1",
+		Port:        0,
 	}
 	c.Graphites = append(c.Graphites, g)
 	c.Logging.WriteTracing = true
